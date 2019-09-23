b19e3cc052fd810ab05d9018ecfa3056be1081a4
Marcel Vanthoor <mail@marcelvanthoor.nl>
2019-09-23 20:28:15 +0200

    Test generation of blockers

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,33 @@ fn main() {
     board.create_start_position();
     print::position(&board);
 
-    for i in 0..64 {
-        println!("{}", i);
-        print::bitboard(board.bb_mask[BB_MASK_P_MOVE_W][i]);
+    // Test generation of all blockers, iterative
+    pub type SuperBit = [u8; 8];
+    let mut super_bit: SuperBit = [0; 8];
+    let mut super_bit_list: Vec<SuperBit> = Vec::new();
+
+    println!();
+    for i in 0..super_bit.len() {
+        if i == 0 {
+            super_bit_list.push(super_bit);
+            super_bit[i] = 1;
+            super_bit_list.push(super_bit);
+            super_bit[i] = 0;
+        } else {
+            for j in 0..super_bit_list.len() {
+                let mut sb = super_bit_list[j];
+                sb[i] = 1;
+                super_bit_list.push(sb);
+            }
+        }
+    }
+
+    for i in 0..super_bit_list.len() {
+        let sb = super_bit_list[i];
+        print!("{}: ", i);
+        for j in 0..sb.len() {
+            print!("{}", sb[j]);
+        }
+        println!();
     }
 }
