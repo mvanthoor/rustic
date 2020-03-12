872acce7401edcd1bc4841b6f32ae03c27f7735b
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-03-12 21:58:09 +0100

    Update rays.rs after testing

--- src/movegenerator/rays.rs
+++ src/movegenerator/rays.rs
@@ -16,13 +16,13 @@ use crate::defs::{Bitboard, FILE_A, FILE_H, RANK_1, RANK_8};
  * If starting on square 28 (E4) in direction UP, it will iterate over the
  * Direction::Up block until done.
  *
- * The ray ends, when bb_square either passes over the edge of the board, or hits
- * a set bit (piece) in bb_in. A piece/square in bb_in will be *included* in the ray,
+ * The ray ends, either when the edge of the board is reached, or it hits a piece.
+ * (bb_square & bb_in > 0). A piece/square in bb_in will be *included* in the ray,
  * because the ray can 'see' it.
  */
 pub fn create_bb_ray(bb_in: Bitboard, square: u8, direction: Direction) -> Bitboard {
-    let mut file = square_on_file_rank(square).0 as i8;
-    let mut rank = square_on_file_rank(square).1 as i8;
+    let mut file = square_on_file_rank(square).0 as usize;
+    let mut rank = square_on_file_rank(square).1 as usize;
     let mut bb_square = 1u64 << square;
     let mut bb_ray = 0;
     let mut done = false;
@@ -30,84 +30,75 @@ pub fn create_bb_ray(bb_in: Bitboard, square: u8, direction: Direction) -> Bitbo
         done = true;
         match direction {
             Direction::Up => {
-                if rank != (RANK_8 as i8) {
+                if rank != RANK_8 {
                     bb_square <<= 8;
                     bb_ray |= bb_square;
                     rank += 1;
-                    done = (rank > RANK_8 as i8) || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             }
             Direction::Right => {
-                if file != (FILE_H as i8) {
+                if file != FILE_H {
                     bb_square <<= 1;
                     bb_ray |= bb_square;
                     file += 1;
-                    done = (file > FILE_H as i8) || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             }
             Direction::Down => {
-                if rank != (RANK_1 as i8) {
+                if rank != RANK_1 {
                     bb_square >>= 8;
                     bb_ray |= bb_square;
                     rank -= 1;
-                    done = (rank < RANK_1 as i8) || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             }
             Direction::Left => {
-                if file != (FILE_A as i8) {
+                if file != FILE_A {
                     bb_square >>= 1;
                     bb_ray |= bb_square;
                     file -= 1;
-                    done = (file < FILE_A as i8) || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             }
             #[rustfmt::skip]
             Direction::UpLeft => {
-                if rank != (RANK_8 as i8) && file != (FILE_A as i8) {
+                if (rank != RANK_8) && (file != FILE_A) {
                     bb_square <<= 7;
                     bb_ray |= bb_square;
                     rank += 1;
                     file -= 1;
-                    done = (rank > RANK_8 as i8)
-                        || (file < FILE_A as i8)
-                        || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             },
             #[rustfmt::skip]
             Direction::UpRight => {
-                if rank != (RANK_8 as i8) && file != (FILE_H as i8) {
+                if (rank != RANK_8) && (file != FILE_H) {
                     bb_square <<= 9;
                     bb_ray |= bb_square;
                     rank += 1;
                     file += 1;
-                    done = (rank > RANK_8 as i8)
-                        || (file > FILE_H as i8)
-                        || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             },
             #[rustfmt::skip]
             Direction::DownRight => {
-                if rank != (RANK_1 as i8) && file != (FILE_H as i8) {
+                if (rank != RANK_1) && (file != FILE_H) {
                     bb_square >>= 7;
                     bb_ray |= bb_square;
                     rank -= 1;
                     file += 1;
-                    done = (rank < RANK_1 as i8)
-                        || (file > FILE_H as i8)
-                        || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             },
             #[rustfmt::skip]
             Direction::DownLeft => {
-                if rank != (RANK_1 as i8) && file != (FILE_A as i8) {
+                if (rank != RANK_1) && (file != FILE_A) {
                     bb_square >>= 9;
                     bb_ray |= bb_square;
                     rank -= 1;
                     file -= 1;
-                    done =
-                        (rank < RANK_1 as i8)
-                        || (file < FILE_A as i8)
-                        || (bb_square & bb_in) > 0;
+                    done = (bb_square & bb_in) > 0;
                 }
             },
         };
