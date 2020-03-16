88b0429b8548929f97e408184d7d2c13a30c49d8
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-03-16 22:24:07 +0100

    Initialize zobrist piece randoms

--- src/board/zobrist.rs
+++ src/board/zobrist.rs
@@ -1,11 +1,34 @@
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
 
-pub fn initialize() {
-    let mut random = SmallRng::from_seed([192; 16]);
+use crate::defs::NR_OF_SQUARES;
 
-    for i in 0..10 {
-        let key = random.gen::<u64>();
-        println!("Key {}: {}", i, key);
+const NSQ: usize = NR_OF_SQUARES as usize;
+const EACH_PIECE: usize = 6;
+const EACH_SIDE: usize = 2;
+
+/* Random number for each side's pieces on each square */
+type PieceRandoms = [[[u64; NSQ]; EACH_PIECE]; EACH_SIDE];
+
+pub struct ZobristRandoms {
+    piece_randoms: PieceRandoms,
+}
+
+impl ZobristRandoms {
+    pub fn new() -> ZobristRandoms {
+        let mut random = SmallRng::from_entropy();
+        let mut zobrist_randoms = ZobristRandoms {
+            piece_randoms: [[[0; NSQ]; EACH_PIECE]; EACH_SIDE],
+        };
+        // Fill piece_randoms for each side, for each piece, on each square.
+        for side in 0..EACH_SIDE {
+            for piece in 0..EACH_PIECE {
+                for square in 0..NSQ {
+                    let key = random.gen::<u64>();
+                    zobrist_randoms.piece_randoms[side][piece][square] = key;
+                }
+            }
+        }
+        zobrist_randoms
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod movegen;
 mod utils;
 
 use board::representation::Board;
-use board::zobrist;
+use board::zobrist::ZobristRandoms;
 use extra::print;
 use movegen::movedefs::{MoveList, MAX_LEGAL_MOVES};
 use movegen::MoveGenerator;
@@ -16,6 +16,7 @@ fn main() {
     let mut board: Board = Board::new(Some(test_pos));
     let mut moves: MoveList = Vec::with_capacity(MAX_LEGAL_MOVES as usize);
     let move_generator: MoveGenerator = MoveGenerator::new();
+    let zobrist_randoms: ZobristRandoms = ZobristRandoms::new();
 
     engine_info();
 
@@ -23,8 +24,6 @@ fn main() {
 
     move_generator.gen_all_moves(&board, &mut moves);
     print::movelist(&moves);
-
-    zobrist::initialize();
 
     println!("Done.");
 }
