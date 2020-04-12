6119150502fa2a23716b5e38a52eb35e3a78fdd2
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-04-13 01:45:01 +0200

    inline make and unmake

--- src/board/domove.rs
+++ src/board/domove.rs
@@ -7,6 +7,7 @@ use crate::movegen::{info, movedefs::Move};
 use crate::utils::bits;
 
 // TODO: Update comments
+#[inline(always)]
 pub fn make(board: &mut Board, m: Move) -> bool {
     // Create the unmake info and store it.
     let mut current_game_state = board.game_state;
@@ -155,6 +156,7 @@ fn move_rook_during_castling(board: &mut Board, king_square: u8) {
 /*** ================================================================================ ***/
 
 // TODO: Update comments
+#[inline(always)]
 pub fn unmake(board: &mut Board) {
     let stored = board.history.pop();
 
