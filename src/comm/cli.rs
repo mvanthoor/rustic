d9b2494ed628e57dfaf09d40c0d45d74c2921445
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-04-15 21:18:39 +0200

    Extend cli with move parsing.

--- src/comm/cli.rs
+++ src/comm/cli.rs
@@ -1,10 +1,13 @@
 use crate::board::representation::Board;
 use crate::defs::ENGINE;
 use crate::extra::{perft, perftsuite};
+use crate::movegen::movedefs::MoveList;
 use crate::utils::parse;
+use if_chain::if_chain;
 use std::{io, io::Write};
 
-const CMD_STR_ERROR: &str = "Command-line i/o error";
+const CMD_STR_ERR_IO: &str = "Command-line i/o error";
+const CMD_STR_ERR_MOVE: &str = "Error in move input.";
 const CMD_QUIT: u64 = 0;
 const CMD_CONTINUE: u64 = 1;
 
@@ -14,11 +17,11 @@ pub fn get_input(board: &mut Board) -> u64 {
     print!("{} > ", ENGINE);
     match io::stdout().flush() {
         Ok(()) => (),
-        Err(error) => panic!("{}: {}", CMD_STR_ERROR, error),
+        Err(error) => panic!("{}: {}", CMD_STR_ERR_IO, error),
     }
     match io::stdin().read_line(&mut input) {
         Ok(_) => parse_input(&mut input, board),
-        Err(error) => panic!("{}: {}", CMD_STR_ERROR, error),
+        Err(error) => panic!("{}: {}", CMD_STR_ERR_IO, error),
     }
 }
 
@@ -29,7 +32,7 @@ fn parse_input(input: &mut String, board: &mut Board) -> u64 {
         "perft" => cmd_perft(board),
         "suite" => cmd_suite(),
         "clear" => cmd_clear(),
-        _ => cmd_parse_move(input),
+        _ => cmd_parse_move(input, board),
     }
 }
 
@@ -47,17 +50,31 @@ fn cmd_suite() -> u64 {
     CMD_CONTINUE
 }
 
-fn cmd_parse_move(input: &mut String) -> u64 {
+fn cmd_parse_move(input: &mut String, board: &Board) -> u64 {
     let length = input.len();
+    let mut from: u8 = 0;
+    let mut to: u8 = 0;
+    let mut error = 0;
+
     if (4..=5).contains(&length) {
-        for (index, c) in input.chars().enumerate() {
-            match index {
-                0 | 2 => println!("file: {}", c),
-                1 | 3 => println!("rank: {}", c),
-                4 => println!("Promotion! {}", c),
-                _ => (),
+        if_chain! {
+            if let Ok(f) = parse::algebraic_square_to_number(&input[0..=1]);
+            if let Ok(t) = parse::algebraic_square_to_number(&input[2..=3]);
+            then {
+                from = f;
+                to = t;
+            } else {
+                error = 1;
             }
         }
+    };
+    if error == 0 {
+        let mut move_list = MoveList::new();
+        board.gen_all_moves(&mut move_list);
+
+        println!("from: {}, to: {}", from, to);
+    } else {
+        println!("{}", CMD_STR_ERR_MOVE);
     }
     CMD_CONTINUE
 }
--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,13 @@ fn main() {
     let test_pos: &str = "rnbqkbnr/pp1ppppp/2p5/8/PP6/8/2PPPPPP/RNBQKBNR w KQkq - 0 1";
     let move_generator = MoveGenerator::new();
     let zobrist_randoms = ZobristRandoms::new();
-    let mut board: Board = Board::new(&zobrist_randoms, &move_generator, Some(test_pos));
+    let mut board: Board = Board::new(&zobrist_randoms, &move_generator, None);
     let mut move_list: MoveList = MoveList::new();
 
     utils::engine_info();
     print::position(&board, None);
 
-    // while cli::get_input(&mut board) != 0 {}
+    while cli::get_input(&mut board) != 0 {}
     // perft::run(&board, 7);
     // perftsuite::run_all_tests();
 }
