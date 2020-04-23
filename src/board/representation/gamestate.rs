f6809ae743f417340460563e9de0e189e4343b94
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-04-23 17:57:36 +0200

    Add #[repr(C)] to GameState struct.

--- src/board/representation/gamestate.rs
+++ src/board/representation/gamestate.rs
@@ -6,11 +6,12 @@ use crate::movegen::movedefs::Move;
 // each game state variable one by one.
 
 #[derive(Clone, Copy)]
+#[repr(C)]
 pub struct GameState {
     pub active_color: u8,
     pub castling: u8,
-    pub en_passant: Option<u8>,
     pub halfmove_clock: u8,
+    pub en_passant: Option<u8>,
     pub fullmove_number: u16,
     pub zobrist_key: u64,
     pub this_move: Move,
