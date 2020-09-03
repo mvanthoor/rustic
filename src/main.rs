a4eab618823913e221d83c6347e3459b822b34f3
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-09-03 12:19:40 +0200

    Integrate clap library

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,29 @@ mod movegen;
 
 // use interface::console;
 use board::{defs::ERR_FEN_PARTS, Board};
+use clap::{App, Arg};
+use defs::{AUTHOR, ENGINE, VERSION};
 use extra::perft;
 use misc::info;
 use movegen::MoveGenerator;
 
 fn main() {
+    let cmd_line = App::new(ENGINE)
+        .version(VERSION)
+        .author(AUTHOR)
+        .about("UCI Chess Engine in Rust.")
+        .arg(
+            Arg::with_name("experiment")
+                .short("x")
+                .long("experiment")
+                .takes_value(true)
+                .help("This argument is being tested."),
+        )
+        .get_matches();
+
+    let x = cmd_line.value_of("experiment").unwrap_or("Not proven.");
+    println!("The value of this experiment is: {}", x);
+
     let test_pos = Some("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
     let move_generator = MoveGenerator::new();
     let mut board: Board = Board::new();
