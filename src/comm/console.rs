23459acdab8180d5e5fd1e39d1b15afca1b54934
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-10-06 11:33:18 +0200

    Refactor Console.rs

--- src/comm/console.rs
+++ src/comm/console.rs
@@ -17,6 +17,8 @@ use std::{
     thread::{self, JoinHandle},
 };
 
+const UNKNOWN_INPUT: &'static str = "Unknown input:";
+
 pub struct Console {
     control_handle: Option<JoinHandle<()>>,
     report_handle: Option<JoinHandle<()>>,
@@ -24,78 +26,24 @@ pub struct Console {
     last_report: Arc<Mutex<CommReport>>,
 }
 
+// Public functions
+impl Console {
+    pub fn new() -> Self {
+        Self {
+            control_handle: None,
+            report_handle: None,
+            control_tx: None,
+            last_report: Arc::new(Mutex::new(CommReport::Nothing)),
+        }
+    }
+}
+
 // Any communication module must implement the trait IComm.
 impl IComm for Console {
     fn init(&mut self, report_tx: Sender<Information>, board: Arc<Mutex<Board>>) {
-        // Create thread-local variables.
-        // ***///
-        let mut t_incoming_data = String::from("");
-        let t_report_tx = report_tx.clone(); // Report incoming data to engine.
-        let t_board = Arc::clone(&board);
-        let t_last_report = Arc::clone(&self.last_report);
-        //***///
-
-        // Create the Reporting thread, which will read incoming data from
-        // stdin, and report it back to the engine's main thread.
-        let reader_handle = thread::spawn(move || {
-            let mut quit = false;
-
-            // Keep running as long as 'quit' is not detected.
-            while !quit {
-                // Get data from stdin and create a report from it.
-                io::stdin()
-                    .read_line(&mut t_incoming_data)
-                    .expect(ErrFatal::READ_IO);
-                let new_report = Console::create_report(&t_incoming_data);
-
-                // Terminate at the end of this iteration.
-                quit = new_report == CommReport::Quit;
-
-                // Check validity of the created report and act accordingly.
-                if new_report.is_valid() {
-                    // Valid. Save as last report, and send it to the engine.
-                    *t_last_report.lock().expect(ErrFatal::LOCK) = new_report.clone();
-                    let information = Information::Comm(new_report.clone());
-                    t_report_tx.send(information).expect(ErrFatal::HANDLE);
-                } else {
-                    // Or give an error message, and print the board again.
-                    print!("Unknown input: {}", t_incoming_data);
-                    *t_last_report.lock().expect(ErrFatal::LOCK) = CommReport::Unknown;
-                    Console::update(&t_last_report, &t_board);
-                }
-
-                // Clear input for next iteration.
-                t_incoming_data = String::from("");
-            }
-        });
-
-        // Store the handle.
-        self.report_handle = Some(reader_handle);
-
-        // ============================================================================
-
-        // Create a channel for the control thread.
-        let (control_tx, control_rx) = crossbeam_channel::unbounded::<CommControl>();
-        let t_last_report = Arc::clone(&self.last_report);
-
-        // Create the control thread.
-        let control_handle = thread::spawn(move || {
-            let mut quit = false;
-
-            while !quit {
-                let control = control_rx.recv().expect(ErrFatal::CHANNEL);
-
-                match control {
-                    CommControl::Quit => quit = true,
-                    CommControl::Update => Console::update(&t_last_report, &board),
-                    CommControl::Write(msg) => println!("{}", msg),
-                }
-            }
-        });
-
-        // Store handle and control sender.
-        self.control_handle = Some(control_handle);
-        self.control_tx = Some(control_tx);
+        // Start threads
+        self.report_thread(report_tx.clone(), Arc::clone(&board));
+        self.control_thread(board);
 
         // Report to engine that init is finished.
         report_tx
@@ -129,15 +77,79 @@ impl IComm for Console {
     }
 }
 
-// Public functions
+// This block implements the Report and Control threads.
 impl Console {
-    pub fn new() -> Self {
-        Self {
-            control_handle: None,
-            report_handle: None,
-            control_tx: None,
-            last_report: Arc::new(Mutex::new(CommReport::Nothing)),
-        }
+    // The Report thread sends incoming data to the engine thread.
+    fn report_thread(&mut self, report_tx: Sender<Information>, board: Arc<Mutex<Board>>) {
+        // Create thread-local variables
+        let mut t_incoming_data = String::from("");
+        let t_report_tx = report_tx.clone(); // Report sender
+        let t_board = Arc::clone(&board);
+        let t_last_report = Arc::clone(&self.last_report);
+
+        // Actual thread creation.
+        let report_handle = thread::spawn(move || {
+            let mut quit = false;
+
+            // Keep running as long as 'quit' is not detected.
+            while !quit {
+                // Get data from stdin and create a report from it.
+                io::stdin()
+                    .read_line(&mut t_incoming_data)
+                    .expect(ErrFatal::READ_IO);
+                let new_report = Console::create_report(&t_incoming_data);
+
+                // Terminate if 'quit' is detected.
+                quit = new_report == CommReport::Quit;
+
+                // Check validity of the created report and act accordingly.
+                if new_report.is_valid() {
+                    // Valid. Save as last report, and send it to the engine.
+                    *t_last_report.lock().expect(ErrFatal::LOCK) = new_report.clone();
+                    let information = Information::Comm(new_report.clone());
+                    t_report_tx.send(information).expect(ErrFatal::HANDLE);
+                } else {
+                    // Or give an error message, and print the board again.
+                    print!("{} {}", UNKNOWN_INPUT, t_incoming_data);
+                    *t_last_report.lock().expect(ErrFatal::LOCK) = CommReport::Unknown;
+                    Console::update(&t_last_report, &t_board);
+                }
+
+                // Clear input for next iteration.
+                t_incoming_data = String::from("");
+            }
+        });
+
+        // Store the handle.
+        self.report_handle = Some(report_handle);
+    }
+
+    fn control_thread(&mut self, board: Arc<Mutex<Board>>) {
+        // Create an incoming channel for the control thread.
+        let (control_tx, control_rx) = crossbeam_channel::unbounded::<CommControl>();
+
+        // Create thread-local variables.
+        let t_last_report = Arc::clone(&self.last_report);
+
+        // Create the control thread.
+        let control_handle = thread::spawn(move || {
+            let mut quit = false;
+
+            // Keep running as long as Quit is not received.
+            while !quit {
+                let control = control_rx.recv().expect(ErrFatal::CHANNEL);
+
+                match control {
+                    CommControl::Quit => quit = true,
+                    CommControl::Update => Console::update(&t_last_report, &board),
+                    CommControl::Write(msg) => println!("{}", msg),
+                }
+            }
+        });
+
+        // Store handle and control sender.
+        self.control_handle = Some(control_handle);
+        self.control_tx = Some(control_tx);
     }
 }
 
