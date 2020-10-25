ed55c08d053c4a1d38b41161ad2f7effa5381273
Marcel Vanthoor <mail@marcelvanthoor.nl>
2020-10-25 16:57:14 +0100

    Add license header to all files

--- src/engine/utils.rs
+++ src/engine/utils.rs
@@ -1,3 +1,25 @@
+/* =======================================================================
+Rustic is a chess playing engine.
+Copyright (C) 2019-2020, Marcel Vanthoor
+
+Rustic is written in the Rust programming language. It is an original
+work, not derived from any engine that came before it. However, it does
+use a lot of concepts which are well-known and are in use by most if not
+all classical alpha/beta-based chess engines.
+
+Rustic is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+Rustic is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+======================================================================= */
+
 use super::{defs::ErrFatal, Engine};
 use crate::{
     board::Board,
