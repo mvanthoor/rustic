98d0572f7c1198349e36dcdbfb5ddc21f3c547fa
Marcel Vanthoor <mail@marcelvanthoor.nl>
2021-03-29 18:38:23 +0200

    Add platform case to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo "Building Rustic. Please wait."
+
+platform=$(uname -a | awk '{print tolower($0)}')
+
+case $platform in
+  "mingw64"*)
+    echo "Windows 64-bit"
+    ;;
+  "mingw32"*)
+    echo "Windows 32-bit"
+    ;;
+  "darwin"*)
+    echo "MacOS"
+    ;;
+esac
