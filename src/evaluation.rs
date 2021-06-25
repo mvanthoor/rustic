70af0f2026ca6ed0040896d7ea3b34e2a89f12ae
Marcel Vanthoor <mail@marcelvanthoor.nl>
2021-06-25 22:12:11 +0200

    Better comments and variable names.

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -40,10 +40,10 @@ impl Evaluation {
         let pst_w_eg = board.game_state.pst_eg[Sides::WHITE] as f32;
         let pst_b_eg = board.game_state.pst_eg[Sides::BLACK] as f32;
 
-        // Get phase
+        // Get phase, from 1 (opening/midgame) to 0 (endgame)
         let phase = Evaluation::phase(PHASE_MIN, PHASE_MAX, board.game_state.phase_value);
 
-        // Mix the tables by interpolation
+        // Mix the tables by taking parts of both mg and eg.
         let pst_w = (pst_w_mg * phase) + (pst_w_eg * (1.0 - phase));
         let pst_b = (pst_b_mg * phase) + (pst_b_eg * (1.0 - phase));
 
@@ -56,8 +56,12 @@ impl Evaluation {
         value
     }
 
+    // Get the phase by using the Linstep method.
     pub fn phase(edge0: i16, edge1: i16, value: i16) -> f32 {
-        let clamp = (value - edge0) as f32 / (edge1 - edge0) as f32;
-        f32::min(1.0, f32::max(0.0, clamp))
+        // Interpolate from edge0 to edge1.
+        let result = (value - edge0) as f32 / (edge1 - edge0) as f32;
+
+        // Clamp the result: don't exceed 1.0 or drop below 0.0.
+        f32::min(1.0, f32::max(0.0, result))
     }
 }
