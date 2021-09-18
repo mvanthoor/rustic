408a7fbca2a2fe59d17b89d65fd12901e1884633
Marcel Vanthoor <mail@marcelvanthoor.nl>
2021-09-18 12:18:55 +0200

    Rename variables

--- src/engine/transposition.rs
+++ src/engine/transposition.rs
@@ -188,42 +188,42 @@ impl<D: IHashData> Bucket<D> {
 
 #[derive(Clone)]
 struct Entry<D> {
-    bucket: [Bucket<D>; NR_OF_BUCKETS],
+    entry: [Bucket<D>; NR_OF_BUCKETS],
 }
 
 impl<D: IHashData + Copy> Entry<D> {
     pub fn new() -> Self {
         Self {
-            bucket: [Bucket::new(); NR_OF_BUCKETS],
+            entry: [Bucket::new(); NR_OF_BUCKETS],
         }
     }
 
     // Store a position in the bucket. Replace the position with the stored
     // lowest depth, as positions with higher depth are more valuable.
     pub fn store(&mut self, verification: u32, data: D, used_entries: &mut usize) {
-        let mut idx_lowest_depth = 0;
+        let mut idx_low = 0;
 
         // Find the index of the entry with the lowest depth.
-        for entry in 1..NR_OF_BUCKETS {
-            if self.bucket[entry].data.depth() < data.depth() {
-                idx_lowest_depth = entry
+        for i in 1..NR_OF_BUCKETS {
+            if self.entry[i].data.depth() < data.depth() {
+                idx_low = i
             }
         }
 
         // If the verification was 0, this entry in the bucket was never
         // used before. Count the use of this entry.
-        if self.bucket[idx_lowest_depth].verification == 0 {
+        if self.entry[idx_low].verification == 0 {
             *used_entries += 1;
         }
 
         // Store.
-        self.bucket[idx_lowest_depth] = Bucket { verification, data }
+        self.entry[idx_low] = Bucket { verification, data }
     }
 
     // Find a position in the bucket, where both the stored verification and
     // depth match the requested verification and depth.
     pub fn find(&self, verification: u32) -> Option<&D> {
-        for e in self.bucket.iter() {
+        for e in self.entry.iter() {
             if e.verification == verification {
                 return Some(&e.data);
             }
