3b834378df8a005bcfa33930ee3c44ef0781ddf7
Marcel Vanthoor <mail@marcelvanthoor.nl>
2021-10-22 17:14:28 +0200

    Implement getters/setters for EngineState

--- src/engine.rs
+++ src/engine.rs
@@ -222,4 +222,36 @@ impl Engine {
         self.comm.send(CommOut::Quit);
         self.quit = true;
     }
+
+    fn is_observing(&self) -> bool {
+        self.state == EngineState::Observing
+    }
+
+    fn is_waiting(&self) -> bool {
+        self.state == EngineState::Waiting
+    }
+
+    fn is_thinking(&self) -> bool {
+        self.state == EngineState::Thinking
+    }
+
+    fn is_analyzing(&self) -> bool {
+        self.state == EngineState::Analyzing
+    }
+
+    fn set_observing(&mut self) {
+        self.state = EngineState::Observing;
+    }
+
+    fn set_waiting(&mut self) {
+        self.state = EngineState::Waiting;
+    }
+
+    fn set_thinking(&mut self) {
+        self.state = EngineState::Thinking;
+    }
+
+    fn set_analyzing(&mut self) {
+        self.state = EngineState::Analyzing;
+    }
 }
