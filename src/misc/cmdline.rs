88af5ed8dbb04284fe9b78fdce84d74bb0a4b3aa
Marcel Vanthoor <mail@marcelvanthoor.nl>
2022-01-31 17:16:09 +0100

    Upgrade libraries

--- src/misc/cmdline.rs
+++ src/misc/cmdline.rs
@@ -33,56 +33,56 @@ struct CmdLineArgs {}
 impl CmdLineArgs {
     // FEN
     const FEN_LONG: &'static str = "fen";
-    const FEN_SHORT: &'static str = "f";
+    const FEN_SHORT: char = 'f';
     const FEN_HELP: &'static str = "Set up the given position";
 
     // Perft
     const PERFT_LONG: &'static str = "perft";
-    const PERFT_SHORT: &'static str = "p";
+    const PERFT_SHORT: char = 'p';
     const PERFT_HELP: &'static str = "Run perft to the given depth";
     const PERFT_DEFAULT: &'static str = "0";
 
     // Interface
     const COMM_LONG: &'static str = "comm";
-    const COMM_SHORT: &'static str = "c";
+    const COMM_SHORT: char = 'c';
     const COMM_HELP: &'static str = "Select communication protocol to use";
     const COMM_VALUES: [&'static str; 2] = ["uci", "xboard"];
     const COMM_DEFAULT: &'static str = "uci";
 
     // Threads
     const THREADS_LONG: &'static str = "threads";
-    const THREADS_SHORT: &'static str = "t";
+    const THREADS_SHORT: char = 't';
     const THREADS_HELP: &'static str = "Number of CPU-threads to use";
     const THREADS_DEFAULT: &'static str = "1";
 
     const HASH_LONG: &'static str = "hash";
-    const HASH_SHORT: &'static str = "h";
+    const HASH_SHORT: char = 'h';
     const HASH_HELP: &'static str = "Transposition Table size in MB";
     const HASH_DEFAULT: &'static str = EngineOptionDefaults::HASH_DEFAULT;
 
     // Quiet (no search stats updates except on depth change)
     const QUIET_LONG: &'static str = "quiet";
-    const QUIET_SHORT: &'static str = "q";
+    const QUIET_SHORT: char = 'q';
     const QUIET_HELP: &'static str = "No intermediate search stats updates";
 
     // Kiwipete
     const KIWI_LONG: &'static str = "kiwipete";
-    const KIWI_SHORT: &'static str = "k";
+    const KIWI_SHORT: char = 'k';
     const KIWI_HELP: &'static str = "Set up KiwiPete position (ignore --fen)";
 
     // Wizardry
     const WIZARDRY_LONG: &'static str = "wizardry";
-    const WIZARDRY_SHORT: &'static str = "w";
+    const WIZARDRY_SHORT: char = 'w';
     const WIZARDRY_HELP: &'static str = "Generate magic numbers";
 
     // Test
     const EPD_TEST_LONG: &'static str = "epdtest";
-    const EPD_TEST_SHORT: &'static str = "e";
+    const EPD_TEST_SHORT: char = 'e';
     const EPD_TEST_HELP: &'static str = "Run EPD Test Suite";
 }
 
 pub struct CmdLine {
-    arguments: ArgMatches<'static>,
+    arguments: ArgMatches,
 }
 
 impl CmdLine {
@@ -148,13 +148,13 @@ impl CmdLine {
         self.arguments.is_present(CmdLineArgs::EPD_TEST_LONG)
     }
 
-    fn get() -> ArgMatches<'static> {
+    fn get() -> ArgMatches {
         let mut app = App::new(About::ENGINE)
             .version(About::VERSION)
             .author(About::AUTHOR)
             .about(About::WEBSITE)
             .arg(
-                Arg::with_name(CmdLineArgs::COMM_LONG)
+                Arg::new(CmdLineArgs::COMM_LONG)
                     .short(CmdLineArgs::COMM_SHORT)
                     .long(CmdLineArgs::COMM_LONG)
                     .help(CmdLineArgs::COMM_HELP)
@@ -163,7 +163,7 @@ impl CmdLine {
                     .possible_values(&CmdLineArgs::COMM_VALUES),
             )
             .arg(
-                Arg::with_name(CmdLineArgs::FEN_LONG)
+                Arg::new(CmdLineArgs::FEN_LONG)
                     .short(CmdLineArgs::FEN_SHORT)
                     .long(CmdLineArgs::FEN_LONG)
                     .help(CmdLineArgs::FEN_HELP)
@@ -171,7 +171,7 @@ impl CmdLine {
                     .default_value(FEN_START_POSITION),
             )
             .arg(
-                Arg::with_name(CmdLineArgs::PERFT_LONG)
+                Arg::new(CmdLineArgs::PERFT_LONG)
                     .short(CmdLineArgs::PERFT_SHORT)
                     .long(CmdLineArgs::PERFT_LONG)
                     .help(CmdLineArgs::PERFT_HELP)
@@ -179,7 +179,7 @@ impl CmdLine {
                     .default_value(CmdLineArgs::PERFT_DEFAULT),
             )
             .arg(
-                Arg::with_name(CmdLineArgs::THREADS_LONG)
+                Arg::new(CmdLineArgs::THREADS_LONG)
                     .short(CmdLineArgs::THREADS_SHORT)
                     .long(CmdLineArgs::THREADS_LONG)
                     .help(CmdLineArgs::THREADS_HELP)
@@ -187,7 +187,7 @@ impl CmdLine {
                     .default_value(CmdLineArgs::THREADS_DEFAULT),
             )
             .arg(
-                Arg::with_name(CmdLineArgs::HASH_LONG)
+                Arg::new(CmdLineArgs::HASH_LONG)
                     .short(CmdLineArgs::HASH_SHORT)
                     .long(CmdLineArgs::HASH_LONG)
                     .help(CmdLineArgs::HASH_HELP)
@@ -195,14 +195,14 @@ impl CmdLine {
                     .default_value(CmdLineArgs::HASH_DEFAULT),
             )
             .arg(
-                Arg::with_name(CmdLineArgs::KIWI_LONG)
+                Arg::new(CmdLineArgs::KIWI_LONG)
                     .long(CmdLineArgs::KIWI_LONG)
                     .short(CmdLineArgs::KIWI_SHORT)
                     .help(CmdLineArgs::KIWI_HELP)
                     .takes_value(false),
             )
             .arg(
-                Arg::with_name(CmdLineArgs::QUIET_LONG)
+                Arg::new(CmdLineArgs::QUIET_LONG)
                     .long(CmdLineArgs::QUIET_LONG)
                     .short(CmdLineArgs::QUIET_SHORT)
                     .help(CmdLineArgs::QUIET_HELP)
@@ -212,14 +212,14 @@ impl CmdLine {
         if cfg!(feature = "extra") {
             app = app
                 .arg(
-                    Arg::with_name(CmdLineArgs::WIZARDRY_LONG)
+                    Arg::new(CmdLineArgs::WIZARDRY_LONG)
                         .short(CmdLineArgs::WIZARDRY_SHORT)
                         .long(CmdLineArgs::WIZARDRY_LONG)
                         .help(CmdLineArgs::WIZARDRY_HELP)
                         .takes_value(false),
                 )
                 .arg(
-                    Arg::with_name(CmdLineArgs::EPD_TEST_LONG)
+                    Arg::new(CmdLineArgs::EPD_TEST_LONG)
                         .short(CmdLineArgs::EPD_TEST_SHORT)
                         .long(CmdLineArgs::EPD_TEST_LONG)
                         .help(CmdLineArgs::EPD_TEST_HELP)
