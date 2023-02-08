74f474efc05bcf45ab8103337c7f91bf7c5d699a
Marcel Vanthoor <mail@marcelvanthoor.nl>
2023-02-08 17:20:47 +0100

    Fix mistake and follow clippy suggestion

--- src/engine/about.rs
+++ src/engine/about.rs
@@ -45,10 +45,10 @@ impl Engine {
     // Print information about the engine.
     pub fn print_about(&self, s: &Settings) {
         let bits = std::mem::size_of::<usize>() * 8;
-        let hash = if s.tt_size <= 0 {
+        let hash = if s.tt_size == 0 {
             String::from("off")
         } else {
-            format!("{} MB", s.tt_size.to_string())
+            format!("{} MB", s.tt_size)
         };
         let threads = if s.threads == 1 {
             String::from("1")
