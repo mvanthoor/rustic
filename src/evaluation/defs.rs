576ad4e0cb37ef9a14f152f2302a731527a86188
Marcel Vanthoor <mail@marcelvanthoor.nl>
2023-12-30 17:52:24 +0100

    Refactor PST (wip)

--- src/evaluation/defs.rs
+++ src/evaluation/defs.rs
@@ -1,4 +1,16 @@
-pub use crate::evaluation::{pst::FLIP, pst::PST_EG, pst::PST_MG, Evaluation};
+pub use crate::evaluation::{pst::FLIP, pst::PST_COLLECTION, Evaluation};
 pub const PHASE_VALUES: [i16; 6] = [0, 1050, 405, 305, 155, 0];
 pub const PHASE_MIN: i16 = 435;
 pub const PHASE_MAX: i16 = 5255;
+
+// PSQT Weight struct First value: middlegame. Second value: endgame.
+pub struct W(pub i16, pub i16);
+impl W {
+    pub fn mg(&self) -> i16 {
+        self.0
+    }
+
+    pub fn eg(&self) -> i16 {
+        self.1
+    }
+}
