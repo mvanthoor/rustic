099efe3493fdce6f79c9703baed979b93cf51243
Marcel Vanthoor <mail@marcelvanthoor.nl>
2024-01-04 17:00:52 +0100

    Refactor error handling

--- src/misc/texel.rs
+++ src/misc/texel.rs
@@ -3,14 +3,18 @@ pub mod defs;
 use defs::DataFileLoadResult;
 use defs::TunerMessages;
 use defs::TunerRunResult;
-use defs::{DataPoint, DataPointParseError, DataPointParseResult};
+use defs::{
+    DataPoint,
+    DataPointParseError::{ErrorInFenString, ErrorInGameResult},
+    DataPointParseResult,
+};
 use std::fs::File;
 use std::path::PathBuf;
 use std::{io, io::BufRead, io::BufReader};
 
 pub struct Tuner {
     data_file_name: PathBuf,
-    data_point: Vec<DataPoint>,
+    data_points: Vec<DataPoint>,
     lowest_mean_squared_error: f32,
 }
 
@@ -18,7 +22,7 @@ impl Tuner {
     pub fn new(data_file_name: PathBuf) -> Self {
         Tuner {
             data_file_name,
-            data_point: vec![],
+            data_points: vec![],
             lowest_mean_squared_error: 0.0,
         }
     }
@@ -42,32 +46,32 @@ impl Tuner {
         let file = File::open(path)?;
         let reader = BufReader::new(file);
 
-        for (i, line) in reader.lines().enumerate() {
-            let i = i + 1;
-            if let Ok(line) = line {
-                match self.parse_epd_line_to_datapoint(line) {
-                    Ok(data_point) => {
-                        self.data_point.push(data_point);
-                        println!("{i} - OK");
-                    }
-                    Err(e) => match e {
-                        DataPointParseError::ErrorInFenString => {
-                            println!("{i} - {}", TunerMessages::ERROR_IN_FEN_STRING)
-                        }
-                        DataPointParseError::ErrorInGameResult => {
-                            println!("{i} - {}", TunerMessages::ERROR_IN_GAME_RESULT)
-                        }
-                    },
-                }
-            } else {
+        for (i, line_result) in reader.lines().enumerate() {
+            if line_result.is_err() {
                 println!("{i} - {}", TunerMessages::ERROR_CANT_READ_LINE);
+                continue;
             }
+
+            let line = line_result.unwrap_or(String::from(""));
+            let parse_result = self.parse_epd_line_to_data_point(line);
+            let data_point = match parse_result {
+                Ok(data_point) => data_point,
+                Err(error) => {
+                    match error {
+                        ErrorInFenString => println!("a"),
+                        ErrorInGameResult => println!("b"),
+                    }
+                    continue;
+                }
+            };
+
+            self.data_points.push(data_point);
         }
 
         Ok(())
     }
 
-    fn parse_epd_line_to_datapoint(&mut self, line: String) -> DataPointParseResult {
+    fn parse_epd_line_to_data_point(&mut self, line: String) -> DataPointParseResult {
         Ok(DataPoint::new(String::from(""), 0.0, 0.0))
     }
 }
