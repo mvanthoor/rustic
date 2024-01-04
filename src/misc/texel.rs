1a151019da5a87a3b6c3f211472264883da94112
Marcel Vanthoor <mail@marcelvanthoor.nl>
2024-01-04 16:21:21 +0100

    Initial version of data file reader

--- src/misc/texel.rs
+++ src/misc/texel.rs
@@ -1,11 +1,12 @@
 pub mod defs;
 
 use defs::DataFileLoadResult;
-use defs::DataPoint;
 use defs::TunerMessages;
+use defs::TunerRunResult;
+use defs::{DataPoint, DataPointParseError, DataPointParseResult};
+use std::fs::File;
 use std::path::PathBuf;
-
-use self::defs::TunerRunResult;
+use std::{io, io::BufRead, io::BufReader};
 
 pub struct Tuner {
     data_file_name: PathBuf,
@@ -24,19 +25,49 @@ impl Tuner {
 
     pub fn run(&mut self) -> TunerRunResult {
         if self.data_file_load().is_err() {
-            println!("{}.", TunerMessages::DATA_FILE_NOT_FOUND);
+            println!("{}", TunerMessages::DATA_FILE_NOT_FOUND);
             return Err(());
         }
 
-        println!("{}.", TunerMessages::DATA_FILE_LOADED);
+        println!("{}", TunerMessages::DATA_FILE_LOADED);
         Ok(())
     }
 
     fn data_file_load(&mut self) -> DataFileLoadResult {
         if !self.data_file_name.exists() {
-            return Err(());
+            return Err(io::Error::from(io::ErrorKind::NotFound));
+        }
+
+        let path = self.data_file_name.as_path();
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+
+        for (i, line) in reader.lines().enumerate() {
+            let i = i + 1;
+            if let Ok(line) = line {
+                match self.parse_epd_line_to_datapoint(line) {
+                    Ok(data_point) => {
+                        self.data_point.push(data_point);
+                        println!("{i} - OK");
+                    }
+                    Err(e) => match e {
+                        DataPointParseError::ErrorInFenString => {
+                            println!("{i} - {}", TunerMessages::ERROR_IN_FEN_STRING)
+                        }
+                        DataPointParseError::ErrorInGameResult => {
+                            println!("{i} - {}", TunerMessages::ERROR_IN_GAME_RESULT)
+                        }
+                    },
+                }
+            } else {
+                println!("{i} - {}", TunerMessages::ERROR_CANT_READ_LINE);
+            }
         }
 
         Ok(())
+    }
+
+    fn parse_epd_line_to_datapoint(&mut self, line: String) -> DataPointParseResult {
+        Ok(DataPoint::new(String::from(""), 0.0, 0.0))
     }
 }
--- src/misc/texel/defs.rs
+++ src/misc/texel/defs.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 pub struct DataPoint {
     fen: String,
     result: f32,
@@ -15,12 +17,15 @@ pub enum DataPointParseError {
     ErrorInGameResult,
 }
 
-pub type DataFileLoadResult = Result<(), ()>;
-pub type DataPointParseResult = Result<(), DataPointParseError>;
+pub type DataFileLoadResult = io::Result<()>;
+pub type DataPointParseResult = Result<DataPoint, DataPointParseError>;
 pub type TunerRunResult = Result<(), ()>;
 
 pub struct TunerMessages;
 impl TunerMessages {
-    pub const DATA_FILE_LOADED: &'static str = "Data file loaded";
-    pub const DATA_FILE_NOT_FOUND: &'static str = "Data file doesn't exist";
+    pub const DATA_FILE_LOADED: &'static str = "Data file loaded.";
+    pub const DATA_FILE_NOT_FOUND: &'static str = "Data file doesn't exist.";
+    pub const ERROR_IN_FEN_STRING: &'static str = "Error in FEN string. Skipped.";
+    pub const ERROR_IN_GAME_RESULT: &'static str = "Error in game result. Skipped.";
+    pub const ERROR_CANT_READ_LINE: &'static str = "Cannot read line. Skipped.";
 }
