552bff7327753b07d27879263d9202dc5663a037
Marcel Vanthoor <mail@marcelvanthoor.nl>
2024-12-31 16:54:41 +0100

    Move search init, send, and shutdown into its own control.rs file

--- librustic/src/search.rs
+++ librustic/src/search.rs
@@ -1,6 +1,7 @@
 // search.rs contains the engine's search routine.
 
 mod alpha_beta;
+mod control;
 pub mod defs;
 mod iter_deep;
 mod qsearch;
@@ -10,19 +11,10 @@ mod transposition;
 mod utils;
 
 use crate::{
-    board::Board,
     engine::defs::{ErrFatal, Information},
-    movegen::MoveGenerator,
-    search::defs::{
-        SearchControl, SearchInfo, SearchParams, SearchRefs, SearchReport, SearchSummary,
-        SearchTerminated,
-    },
-    search::transposition::{SearchData, TT},
+    search::defs::{SearchControl, SearchRefs, SearchReport, SearchSummary},
 };
-use std::{
-    sync::{mpsc::channel, mpsc::Sender, Arc, Mutex},
-    thread::{self, JoinHandle},
-};
+use std::{sync::mpsc::Sender, thread::JoinHandle};
 
 pub struct Search {
     handle: Option<JoinHandle<()>>,
@@ -40,117 +32,6 @@ impl Search {
         Self {
             handle: None,
             control_tx: None,
-        }
-    }
-
-    pub fn init(
-        &mut self,
-        report_tx: Sender<Information>, // Used to send information to engine.
-        board: Arc<Mutex<Board>>,       // Arc pointer to engine's board.
-        mg: Arc<MoveGenerator>,         // Arc pointer to engine's move generator.
-        tt: Arc<Mutex<TT<SearchData>>>,
-    ) {
-        // Set up a channel for incoming commands
-        let (control_tx, control_rx) = channel();
-
-        // Create thread-local variables.
-        let t_report_tx = report_tx;
-
-        // Create the search thread.
-        let h = thread::spawn(move || {
-            // Create thread-local variables.
-            let arc_board = Arc::clone(&board);
-            let arc_mg = Arc::clone(&mg);
-            let arc_tt = Arc::clone(&tt);
-            let mut search_params = SearchParams::new();
-
-            let mut quit = false;
-            let mut halt = true;
-
-            // As long as the search isn't quit, keep this thread alive.
-            while !quit {
-                // Inform the engine that we are now ready to search.
-                t_report_tx
-                    .send(Information::Search(SearchReport::Ready))
-                    .expect(ErrFatal::CHANNEL);
-
-                // Wait for the next incoming command from the engine.
-                let cmd = control_rx.recv().expect(ErrFatal::CHANNEL);
-
-                // And react accordingly.
-                match cmd {
-                    SearchControl::Start(sp) => {
-                        search_params = sp;
-                        halt = false; // This will start the search.
-                    }
-                    SearchControl::Stop | SearchControl::Abandon => halt = true,
-                    SearchControl::Quit => quit = true,
-                    SearchControl::Nothing => (),
-                }
-
-                // Search isn't halted and not going to quit.
-                if !halt && !quit {
-                    // Copy the current board to be used in this thread.
-                    let mtx_board = arc_board.lock().expect(ErrFatal::LOCK);
-                    let mut board = mtx_board.clone();
-                    std::mem::drop(mtx_board);
-
-                    // Create a place to put search information
-                    let mut search_info = SearchInfo::new();
-
-                    // Create references to all needed information and structures.
-                    let mut search_refs = SearchRefs {
-                        board: &mut board,
-                        mg: &arc_mg,
-                        tt: &arc_tt,
-                        search_params: &mut search_params,
-                        search_info: &mut search_info,
-                        control_rx: &control_rx,
-                        report_tx: &t_report_tx,
-                    };
-
-                    // Start the search using Iterative Deepening.
-                    let (best_move, termination) = Search::iterative_deepening(&mut search_refs);
-
-                    // if we didn't abandon the search, return a best move.
-                    if termination != SearchTerminated::Abandoned {
-                        let information = Information::Search(SearchReport::Finished(best_move));
-                        t_report_tx.send(information).expect(ErrFatal::CHANNEL);
-                    }
-
-                    // We halt (stop) or quit the search according to the
-                    // termination condition.
-                    match termination {
-                        SearchTerminated::Stopped | SearchTerminated::Abandoned => {
-                            halt = true;
-                        }
-                        SearchTerminated::Quit => {
-                            halt = true;
-                            quit = true;
-                        }
-                        SearchTerminated::Nothing => (),
-                    }
-                }
-            }
-        });
-
-        // Store the thread's handle and command sender.
-        self.handle = Some(h);
-        self.control_tx = Some(control_tx);
-    }
-
-    // This function is used to send commands into the search thread.
-    pub fn send(&self, cmd: SearchControl) {
-        if let Some(tx) = &self.control_tx {
-            tx.send(cmd).expect(ErrFatal::CHANNEL);
-        }
-    }
-
-    // After sending the quit command, the engine calls this function to
-    // wait for the search to shut down.
-    pub fn shutdown(&mut self) {
-        if let Some(h) = self.handle.take() {
-            h.join().expect(ErrFatal::THREAD);
         }
     }
 }
