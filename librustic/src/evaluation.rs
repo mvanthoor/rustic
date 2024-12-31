787bd36a5af9872929ffe136d0b62b01cdb555f7
Marcel Vanthoor <mail@marcelvanthoor.nl>
2024-12-31 16:24:07 +0100

    Move evaluation function to its own file

--- librustic/src/evaluation.rs
+++ librustic/src/evaluation.rs
@@ -1,22 +1,7 @@
 pub mod defs;
+mod eval;
 mod phase;
 mod psqt;
 mod weights;
 
-use crate::{board::Board, defs::Sides};
-
 pub struct Evaluation;
-impl Evaluation {
-    pub fn evaluate_position(board: &Board) -> i16 {
-        // Determine the side which is evaluating.
-        let side = board.game_state.active_color as usize;
-
-        // Establish base evaluation value by PST score.
-        let mut value = Evaluation::psqt_score(board);
-
-        // Flip point of view if black is evaluating.
-        value = if side == Sides::BLACK { -value } else { value };
-
-        value
-    }
-}
