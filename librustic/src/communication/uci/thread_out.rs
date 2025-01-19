acac3e27df60de40c27d9a5f3d76583edd7e7899
Marcel Vanthoor <mail@marcelvanthoor.nl>
2025-01-19 11:21:44 +0100

    Update comments

--- librustic/src/communication/uci/thread_out.rs
+++ librustic/src/communication/uci/thread_out.rs
@@ -12,18 +12,28 @@ use std::{
 };
 
 impl Uci {
-    // The control thread receives commands from the engine thread.
+    // The UCI output thread receives information from the engine thread.
     pub fn output_thread(&mut self, features: Arc<Vec<Feature>>) {
-        // Create an incoming channel for the output thread.
+        // Create an incoming channel where the information is received.
         let (transmitter_for_engine, received_from_engine) = channel::<EngineOutput>();
+
+        // Clone this because we can't capture variables internal to the
+        // struct. We only need this only once as a reply to the "id"
+        // command when the engine has started.
         let about = self.about.clone();
 
-        // Create the output thread.
+        // Create the output thread. This will print information sent by
+        // the engine thread to stdio. The information will either end up
+        // on screen, or be captured by a GUI depending on how the engine
+        // is being run.
         let thread = thread::spawn(move || {
             loop {
+                // The receiver will block the thread until information is received.
                 let print_to_stdio = received_from_engine.recv().expect(ErrFatal::CHANNEL);
 
-                // Perform command as sent by the engine thread.
+                // EngineOutput could also contain output destined for
+                // other protocols than UCI. We are only interested in the
+                // UCI-part in this thread.
                 if let EngineOutput::Uci(cmd) = print_to_stdio {
                     match cmd {
                         UciOut::Id => {
@@ -33,18 +43,19 @@ impl Uci {
                         }
                         UciOut::ReadyOk => print::readyok(),
                         UciOut::InfoString(message) => print::info_string(&message),
-                        UciOut::Quit => break,
                         UciOut::Custom(message) => print::custom(&message),
                         UciOut::SearchSummary(summary) => print::search_summary(&summary),
                         UciOut::SearchCurrMove(current) => print::search_currmove(&current),
                         UciOut::SearchStats(stats) => print::search_stats(&stats),
                         UciOut::BestMove(bestmove) => print::best_move(&bestmove),
+                        UciOut::Quit => break, // This will shut down the input thread.
                     }
                 }
             }
         });
 
-        // Store handle and control sender.
+        // Store the thread's handle and output transmitter for use by the
+        // engine thread.
         self.output_thread = Some(thread);
         self.output_write = Some(transmitter_for_engine);
     }
