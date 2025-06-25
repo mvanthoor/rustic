47007e7c92721c30f257a9441b09b00ecfd78f1f
Matt <39347872+Chaaronn@users.noreply.github.com>
2025-06-25 07:52:05 +0100

    introduced the ASPIRATION_WINDOW constant in the search definitions to configure the aspiration search range
    Implemented aspiration windows in iterative_deepening so each iteration starts with a window around the previous evaluation and falls back to a full window if needed

--- src/search/iter_deep.rs
+++ src/search/iter_deep.rs
@@ -22,7 +22,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 ======================================================================= */
 
 use super::{
-    defs::{SearchMode, SearchRefs, SearchResult, INF},
+    defs::{SearchMode, SearchRefs, SearchResult, INF, ASPIRATION_WINDOW},
     ErrFatal, Information, Search, SearchReport, SearchSummary,
 };
 use crate::{defs::MAX_PLY, movegen::defs::Move};
@@ -34,6 +34,7 @@ impl Search {
         let mut best_move = Move::new(0);
         let mut root_pv: Vec<Move> = Vec::new();
         let mut stop = false;
+        let mut prev_eval: i16 = 0;
         let is_game_time = refs.search_params.is_game_time();
 
         if is_game_time {
@@ -48,15 +49,38 @@ impl Search {
             }
         }
 
-        let alpha: i16 = -INF;
-        let beta: i16 = INF;
+        // let alpha: i16 = -INF;
+        // let beta: i16 = INF;
 
         refs.search_info.timer_start();
         while (depth <= MAX_PLY) && (depth <= refs.search_params.depth) && !stop {
             refs.search_info.depth = depth;
             refs.search_info.root_analysis.clear();
 
-            let eval = Search::alpha_beta(depth, alpha, beta, &mut root_pv, refs);
+            //let eval = Search::alpha_beta(depth, alpha, beta, &mut root_pv, refs);
+
+            let mut alpha = if depth > 1 {
+                prev_eval - ASPIRATION_WINDOW
+            } else {
+                -INF
+            };
+            let mut beta = if depth > 1 {
+                prev_eval + ASPIRATION_WINDOW
+            } else {
+                INF
+            };
+
+            root_pv.clear();
+            let mut eval = Search::alpha_beta(depth, alpha, beta, &mut root_pv, refs);
+
+            if (eval <= alpha) || (eval >= beta) {
+                alpha = -INF;
+                beta = INF;
+                root_pv.clear();
+                eval = Search::alpha_beta(depth, alpha, beta, &mut root_pv, refs);
+            }
+            prev_eval = eval;
+
 
             if !refs.search_info.interrupted() {
                 if !root_pv.is_empty() {
