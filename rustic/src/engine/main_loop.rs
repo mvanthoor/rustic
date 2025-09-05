6171ac592902dd4cc4742d5b6d47530e9fb0519a
Marcel Vanthoor <mail@marcelvanthoor.nl>
2025-09-05 20:06:38 +0200

    Change the main loop to match the simplification of the previous commit.

--- rustic/src/engine/main_loop.rs
+++ rustic/src/engine/main_loop.rs
@@ -4,14 +4,16 @@ use std::sync::{Arc, mpsc::channel};
 
 impl Engine {
     pub fn main_loop(&mut self) {
-        // Set up a channel for incoming information.
+        // Set up a channel for incoming information. Rules for channels:
+        // When creating a new one between two threads, then give away the
+        // transmitter, and keep the receiver.
         let (info_tx, info_rx) = channel::<EngineInput>();
 
         // Store the information receiver in the engine for use in other functions.
         self.info_rx = Some(info_rx);
 
         // Initialize Communications and Search modules.
-        self.comm.init(info_tx.clone(), Arc::clone(&self.features));
+        self.comm.init(info_tx.clone());
         self.search
             .init(info_tx, Arc::clone(&self.board), Arc::clone(&self.mg));
 
